cd353923adf3e72176690fb993a9f0784df484f3
gi1235 <146915283+gi1235@users.noreply.github.com>
2024-10-26 23:58:44 +0900

    계산기 가운데 부분 그래픽 구현

--- calc/Gui.java
+++ calc/Gui.java
@@ -4,10 +4,10 @@ import java.awt.*;
 public class Gui extends JFrame{
     Gui(){
         setTitle("계산기");
-        setSize(400,600);
+        setSize(500,600);
         setLayout(new GridLayout(3,0));
         north();
-
+        center();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
@@ -27,5 +27,35 @@ public class Gui extends JFrame{
         mainPanel.add(text);
 
         add(mainPanel);
+    }
+
+    void center(){
+        JPanel panel = new JPanel(new GridLayout(0,4));
+
+        JButton persent = new JButton("%");
+        panel.add(persent);
+
+        JButton clearEntry = new JButton("CE");
+        panel.add(clearEntry);
+
+        JButton clear = new JButton("C");
+        panel.add(clear);
+
+        JButton backSpace = new JButton("Backspace");
+        panel.add(backSpace);
+
+        JButton n7 = new JButton("7");
+        panel.add(n7);
+
+        JButton n8 = new JButton("8");
+        panel.add(n8);
+
+        JButton n9 = new JButton("9");
+        panel.add(n9);
+
+        JButton multiply = new JButton("X");
+        panel.add(multiply);
+
+        add(panel);
     }
 }
