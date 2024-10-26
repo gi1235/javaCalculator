e358574951db069caf6960d7e2c6d49ed577573c
gi1235 <146915283+gi1235@users.noreply.github.com>
2024-10-26 22:34:23 +0900

    기본 구성 제작

---
+++ calc/Gui.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Gui extends JFrame{
+    Gui(){
+        setTitle("계산기");
+        setSize(400,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
