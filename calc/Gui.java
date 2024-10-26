e43848d7eb651864cdafae785c4396898a8b968d
gi1235 <146915283+gi1235@users.noreply.github.com>
2024-10-26 22:59:28 +0900

    계산기 윗부분 그래픽 구현

--- calc/Gui.java
+++ calc/Gui.java
@@ -1,10 +1,31 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Gui extends JFrame{
     Gui(){
         setTitle("계산기");
         setSize(400,600);
+        setLayout(new GridLayout(3,0));
+        north();
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    void north(){
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        
+        JPanel panel = new JPanel();
+        JButton option = new JButton("설정");
+        JLabel basic = new JLabel("표준");
+        panel.add(option);
+        panel.add(basic);
+        mainPanel.add("North", panel);
+
+        JTextArea text = new JTextArea();
+        text.setEnabled(false);
+        mainPanel.add(text);
+
+        add(mainPanel);
     }
 }
